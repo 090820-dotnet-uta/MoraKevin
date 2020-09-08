921701924bad323ffa02d8176a8427810afb2db9
k-mora <kevin.mora.2016@gmail.com>
2020-09-08 16:35:47 -0700

    Creates hello world for Kevin Mora

---
+++ HelloWorldDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kevin!");
+            Console.WriteLine("What is you name?");
+            var input = Console.ReadLine();
+            Console.Write($"Hello {input}!");
+        }
+    }
+}
