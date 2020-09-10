bb0b33f53eec7b42f3a36b88a129dd775b9bea53
k-mora <kevin.mora.2016@gmail.com>
2020-09-10 10:14:59 -0500

    Re-added coding challenges

---
+++ CodingChallenges/Week1/1_StringManipulationChallenge - StudentCopy/StringManipulationChallenge/StringManipulationChallenge.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace StringManipulationChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInputString; //this will hold your users message
+            int elementNum;         //this will hold the element number within the messsage that your user indicates
+            char char1;             //this will hold the char value that your user wants to search for in the message.
+            string fName;           //this will hold the users first name
+            string lName;           //this will hold the users last name
+            string userFullName;    //this will hold the users full name;
+            
+            //
+            //
+            //implement the required code here and within the methods below.
+            // 
+            //
+
+            System.Console.WriteLine("Please enter your message and press enter");
+            userInputString = System.Console.ReadLine();
+            System.Console.WriteLine("Please enter a number LESS THAN the length of your string and press enter");
+            string number = System.Console.ReadLine();
+            int.TryParse(number, out elementNum);
+
+            System.Console.WriteLine("StringToUpper produces: ");
+            StringToUpper(userInputString);
+            System.Console.WriteLine("\n");
+            
+            System.Console.WriteLine("StringToLower produces: ");
+            StringToLower(userInputString);
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("StringTrim produces: ");
+            StringTrim(userInputString);
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("StringSubstring produces: ");
+            StringSubstring(userInputString, elementNum);
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("For which character should I search in your original message?");
+            string c = System.Console.ReadLine();
+            char1 = char.Parse(c);
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("SearchChar produces: ");
+            int index = SearchChar(userInputString, char1);
+            System.Console.WriteLine("Char at Index: " +  index.ToString());
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("Please enter your first name");
+            fName = System.Console.ReadLine();
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("Please enter your last name");
+            lName = System.Console.ReadLine();
+            System.Console.WriteLine("\n");
+
+            System.Console.WriteLine("ConcatName produces: ");
+            userFullName = ConcatNames(fName, lName);
+            System.Console.WriteLine("User full name: " + userFullName);
+
+
+
+
+
+        }
+
+        // This method has one string parameter. 
+        // It will:
+        // 1) change the string to all upper case, 
+        // 2) print the result to the console and 
+        // 3) return the new string.
+        public static string StringToUpper(string x){
+            string s = x.ToUpper();
+            System.Console.WriteLine(s);
+            return s;
+        }
+
+        // This method has one string parameter. 
+        // It will:
+        // 1) change the string to all lower case, 
+        // 2) print the result to the console and 
+        // 3) return the new string.        
+        public static string StringToLower(string x){
+            string s = x.ToLower();
+            System.Console.WriteLine(s);
+            return s;
+        }
+        
+        // This method has one string parameter. 
+        // It will:
+        // 1) trim the whitespace from before and after the string, 
+        // 2) print the result to the console and 
+        // 3) return the new string.
+        public static string StringTrim(string x){
+            string s = "";
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i].ToString() != " ") 
+                {
+                    s = x.Substring(i);
+                    break;
+                }
+            }
+            string t = s;
+            for (int i = s.Length - 1; i > 0; i--)
+             {
+                if(s[i].ToString() != " ") 
+                {
+                    t = s.Substring(0, i + 1);
+                    break;
+                }
+            }
+            System.Console.WriteLine(t);
+            return t;
+        }
+        
+        // This method has two parameters, one string and one integer. 
+        // It will:
+        // 1) get the substring based on the integer received, 
+        // 2) print the result to the console and 
+        // 3) return the new string.
+        public static string StringSubstring(string x, int elementNum){
+            string s = x.Substring(elementNum);
+            System.Console.WriteLine(s);
+            return s;
+
+        }
+
+        // This method has two parameters, one string and one char.
+        // It will:
+        // 1) search the string parameter for the char parameter
+        // 2) return the index of the char.
+        public static int SearchChar(string userInputString, char x){
+            String s = userInputString;
+            int index = -1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == x)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index == -1)
+            {
+                System.Console.WriteLine("This string does not contain that char");
+            }
+            return index;
+        }
+
+        // This method has two string parameters.
+        // It will:
+        // 1) concatenate the two strings with a space between them.
+        // 2) return the new string.
+        public static string ConcatNames(string fName, string lName){
+            String s = fName + " " + lName;
+            return s;
+        }
+
+
+
+    }//end of program
+}
---
+++ CodingChallenges/Week1/4_Methods/4_MethodsChallenge-StudentCopy/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace _4_MethodsChallenge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //1
+            string name = GetName();
+            GreetFriend(name);
+
+            //2
+            double result1 = GetNumber();
+            double result2 = GetNumber();
+            int action1 = GetAction();
+            double result3 = DoAction(result1, result2, action1);
+
+            System.Console.WriteLine($"The result of your mathematical operation is {result3}.");
+
+
+        }
+
+        public static string GetName()
+        {
+            System.Console.Write("Please enter your name.");
+            string name = System.Console.ReadLine();
+            return name;
+        }
+
+        public static void GreetFriend(string name)
+        {
+            //Greeting should be: Hello, nameVar. You are my friend
+            //Ex: Hello, Jim. You are my friend
+            System.Console.WriteLine($"Hello, {name}. You are my friend");
+        }
+
+        public static double GetNumber()
+        {
+            //Should throw FormatException if the user did not input a number
+            double d;
+            System.Console.WriteLine("Please input a number");
+            string input = System.Console.ReadLine();
+            if (double.TryParse(input, out d))
+            {
+                System.Console.WriteLine("Thanks!");
+            }
+            else{
+                throw new FormatException("I need a number!");
+            }
+            return d;
+        }
+
+        public static int GetAction()
+        {   int x;
+            while (true)
+            {
+                System.Console.WriteLine("Please enter 1 for addittion, 2 for subtraction, 3 for multiplication, or 4 for division.");
+                string s = System.Console.ReadLine();
+                if(int.TryParse(s, out x))
+                {
+                    if(x > 0 && x < 5){
+                        break;
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("You must enter a number between the specified range!");
+                    }
+                }
+                else{
+                    System.Console.WriteLine("You must enter a number!");
+                }
+            }
+            return x;
+        }
+
+        public static double DoAction(double x, double y, int z)
+        { double value = 0;
+            if (z < 1 || z > 4){
+                throw new FormatException("Z is not 1, 2, 3, or 4");
+            }
+            if (z == 1)
+            {
+                value = x + y;
+            }
+            else if (z == 2)
+            {
+                value = y - x;
+            }
+            else if (z == 3)
+            {
+                value = x * y;
+            }
+            else if (z == 4)
+            {
+                value = x / y;
+            }
+            return value;
+        }
+    }
+}
